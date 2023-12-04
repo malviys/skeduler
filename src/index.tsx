cf082fc85a88c62d71bcad0027f24f7c311cd6b2
malviys <saurabhmalvia997@gmail.com>
2023-12-05 02:13:45 +0530

    refactor: Render scheduler

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import Scheduler from "./ui/scheduler";
+import { useScheduler } from "./hooks/scheduler";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
 );
+
+function App() {
+    const { init } = useScheduler("scheduler");
+
+    React.useLayoutEffect(() => {
+        init();
+    }, [init]);
+
+    const style: React.CSSProperties = {
+        height: "100vh",
+        width: "100%",
+    };
+
+    return (
+        <div style={style}>
+            <Scheduler name="scheduler" view="week" />
+        </div>
+    );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
