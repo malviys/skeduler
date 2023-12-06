40580f5d7e12638c52bc8ec30687aaf5c7659cbb
malviys <saurabhmalvia997@gmail.com>
2023-12-06 23:32:59 +0530

    feat(hook): Add functions to update grid cell

--- src/hooks/data/state.ts
+++ src/hooks/data/state.ts
@@ -2,7 +2,7 @@ import React from "react";
 
 import * as actions from "../../data/store/action";
 import { useDispatch, useSelector } from "../../data/store/store";
-import type { TEventWithExtras, THeader } from "../../data/store/type";
+import type { TCell, TEventWithExtras, TGrid, THeader, TView } from "../../data/store/type";
 
 export function useSchedulerInternalState(name: string) {
     const dispatch = useDispatch();
@@ -11,6 +11,7 @@ export function useSchedulerInternalState(name: string) {
     const mounted = useSelector((state) => state[name]?.mounted || false);
     const events = useSelector((state) => state[name]?.events || []);
     const headers = useSelector((state) => state[name]?.headers || []);
+    const grid = useSelector((state) => state[name]?.grid || {});
 
     const init = React.useCallback(
         (params?: { events?: TEventWithExtras[]; headers?: THeader[] }) => {
@@ -27,7 +28,7 @@ export function useSchedulerInternalState(name: string) {
         (headers: THeader[]) => {
             dispatch(actions.setHeaders(name, headers));
         },
-        [name],
+        [name, dispatch],
     );
 
     const setEvents = React.useCallback(
@@ -37,10 +38,62 @@ export function useSchedulerInternalState(name: string) {
         [name, dispatch],
     );
 
+    const setView = React.useCallback(
+        (view: TView) => {
+            dispatch(actions.setView(name, view));
+        },
+        [name, dispatch],
+    );
+
+    const setMounted = React.useCallback(() => {
+        dispatch(actions.setMounted(name));
+    }, [name, dispatch]);
+
+    const updateGridCellByIndex = React.useCallback(
+        (index: number, headerId: string | number, data: ((data: TCell) => Partial<TCell>) | Partial<TCell>) => {
+            grid[headerId][index] = {
+                ...grid[headerId][index],
+                ...(typeof data === "function" ? data(grid[headerId][index]) : data),
+            };
+
+            dispatch(actions.setGrid(name, { ...grid }));
+        },
+        [name, grid, dispatch],
+    );
+
+    const updateGridCellById = React.useCallback(
+        (id: string, headerId: string | number, data: Partial<TCell>) => {
+            const cellIndex = grid[headerId].findIndex((cell) => cell.id === id);
+
+            if (cellIndex >= 0) {
+                grid[headerId][cellIndex] = {
+                    ...grid[headerId][cellIndex],
+                    ...data,
+                };
+            }
+
+            dispatch(actions.setGrid(name, { ...grid }));
+        },
+        [name, grid, dispatch],
+    );
+
     /** Clears scheduer state */
     const clear = React.useCallback(() => {
         dispatch(actions.clear(name));
-    }, [name]);
+    }, [name, dispatch]);
 
-    return { initialized, mounted, events, headers, init, setHeaders, setEvents, clear };
+    return {
+        initialized,
+        mounted,
+        events,
+        headers,
+        grid,
+        init,
+        setHeaders,
+        setEvents,
+        setView,
+        setMounted,
+        updateGridCellByIndex,
+        clear,
+    };
 }
