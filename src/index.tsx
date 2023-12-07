a6f401f5870053b95bbfbb791cc155462d8e5a03
malviys <saurabhmalvia997@gmail.com>
2023-12-08 00:13:15 +0530

    chore: Add dummy events

--- src/index.tsx
+++ src/index.tsx
@@ -13,11 +13,32 @@ root.render(
 );
 
 function App() {
-    const { init } = useScheduler("scheduler");
+    const { init, setEvents } = useScheduler("scheduler");
 
     React.useLayoutEffect(() => {
         init();
     }, [init]);
+
+    React.useEffect(() => {
+        setEvents([
+            {
+                id: 1,
+                title: "Event 1",
+                start: new Date(new Date().setHours(11)),
+                end: new Date(new Date().setHours(12)),
+                group: [],
+                color: "red",
+            },
+            {
+                id: 2,
+                title: "Event 2",
+                start: new Date(new Date().setHours(11)),
+                end: new Date(new Date().setHours(12)),
+                group: [],
+                color: "yellow",
+            },
+        ]);
+    }, [setEvents]);
 
     const style: React.CSSProperties = {
         height: "100vh",
