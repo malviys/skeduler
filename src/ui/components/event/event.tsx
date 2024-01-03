22aa4dd109cd7bfcc62f31abcf4ef4280140bf6d
malviys <saurabhmalvia997@gmail.com>
2024-01-04 01:21:31 +0530

    fix: decoupling event and draggable-event

--- src/ui/components/event/event.tsx
+++ src/ui/components/event/event.tsx
@@ -1,50 +1,42 @@
 import { SyntheticListenerMap } from "@dnd-kit/core/dist/hooks/utilities";
-import { memo, useMemo } from "react";
+import Color from "color";
+import React from "react";
 
-import { TSchedulerEvent, TSchedulerEventWithExtras } from "../../../data/store/types";
-import Draggable from "../draggable/draggable";
+import { TSchedulerEventWithExtras } from "../../../data/store/types";
 import { mergeClasses } from "../../../utils/styles";
 
 import classes from "./event.module.scss";
+import Draggable from "../draggable/draggable";
 
 type EventProps = {
     event: TSchedulerEventWithExtras;
     renderEvent?: (event: TSchedulerEventWithExtras, options: { dragHandler: SyntheticListenerMap }) => JSX.Element;
 };
 
-function Event(props: EventProps) {
+export const DraggableEvent = React.memo(function (props: EventProps) {
     const { event, renderEvent } = props;
 
-    const placeholderEvent = useMemo(() => {
-        const {
-            color,
-            title,
-            _extras: { visibility, collisions },
-        } = event;
+    const renderingEvent = React.useMemo(() => {
+        function Wrapper(props: { children: React.ReactNode }) {
+            const { children } = props;
 
-        return (
-            <div
-                style={{
-                    width: `calc((163.27px - 2 * var(--scheduler-event-padding)) / ${collisions.size + 1})`,
-                    height: "calc(100px - 2 * var(--scheduler-event-padding))",
-                    backgroundColor: color,
-                    filter: visibility === "faded" ? "brightness(0.8)" : 'none',
-                }}
-                className={mergeClasses(classes.container)}
-            >
-                <span
-                    style={{
-                        width: "6px",
-                        borderRadius: "3px",
-                        backgroundColor: "white",
-                    }}
-                />
-                <div style={{ verticalAlign: "center" }}>
-                    <span>{title}</span>
+            return (
+                <div style={{ width: "150px", height: "98px", padding: "0.125em", cursor: "grab" }}>
+                    {children}
                 </div>
-            </div>
-        );
-    }, [event]);
+            );
+        }
+
+        if (!renderEvent) {
+            return (
+                <Wrapper>
+                    <Event event={event} />
+                </Wrapper>
+            );
+        }
+
+        return (listeners: SyntheticListenerMap) => <Wrapper>{renderEvent(event, { dragHandler: listeners })}</Wrapper>;
+    }, [event, renderEvent]);
 
     return (
         <Draggable
@@ -52,9 +44,57 @@ function Event(props: EventProps) {
             id={`draggable_scheduler_event_${event.id}`}
             data={event}
         >
-            {renderEvent ? (listeners) => renderEvent(event, { dragHandler: listeners }) : <>{placeholderEvent}</>}
+            {renderingEvent}
         </Draggable>
+    );
+});
+
+function Event(props: Omit<EventProps, "renderEvent"> & { dragging?: boolean }) {
+    const {
+        event: { title, color, start, end, _extras: { visibility } },
+        dragging,
+    } = props;
+
+    const eventBgColor = Color(color).lighten(0.6).toString();
+    const handleColor = Color(color).darken(0.6).toString();
+    const fontColor = Color(color).isLight() ? "#000000" : "#FFFFFF";
+    const filter = `${visibility === "faded" ? "brightness(0.8)" : "none"}`;
+
+    return (
+        <div
+            style={{
+                width: "100%",
+                height: "100%",
+                backgroundColor: eventBgColor,
+                color: fontColor,
+                filter,
+                cursor: dragging ? "grabbing" : undefined,
+            }}
+            className={mergeClasses(classes.container)}
+        >
+            <span
+                style={{
+                    width: "0.125em",
+                    borderRadius: "0.25em",
+                    backgroundColor: handleColor,
+                    margin: "0.125em",
+                }}
+            />
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    rowGap: "0.25em",
+                    padding: "0.25em 0",
+                }}
+            >
+                <span style={{ display: "block", fontSize: "12pt" }}>{title}</span>
+                <span style={{ display: "block", fontSize: "8pt" }}>
+                    {start.format("HH:mm")}&nbsp;-&nbsp;{end.format("HH:mm")}
+                </span>
+            </div>
+        </div>
     );
 }
 
-export default memo(Event);
+export default React.memo(Event);
