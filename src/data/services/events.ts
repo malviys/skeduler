93d2f874bb34d426f7b7bf95422ad8c4502679a2
malviys <saurabhmalvia997@gmail.com>
2024-01-13 23:31:10 +0530

    fix: gen events with correct week dates

--- src/data/services/events.ts
+++ src/data/services/events.ts
@@ -1,37 +1,35 @@
-export async function fetchEvents() {
-    return await new Promise((res, _rej) => {
-        const curr = new Date();
+import dayjs from "dayjs";
 
-        const result = [
-            {
-                id: "1",
-                title: "Event 1",
-                start: new Date(curr.getFullYear(), curr.getMonth(), curr.getDate(), 13, 0, 0, 0),
-                end: new Date(curr.getFullYear(), curr.getMonth(), curr.getDate(), 14, 0, 0, 0),
-                group: ["0"],
-                color: "red",
-            },
-            {
-                id: "2",
-                title: "Event 2",
-                start: new Date(curr.getFullYear(), curr.getMonth(), curr.getDate() + 1, 11, 0, 0, 0),
-                end: new Date(curr.getFullYear(), curr.getMonth(), curr.getDate() + 1, 12, 0, 0, 0),
-                group: ["6"],
-                color: "yellow",
-            },
-            {
-                id: "3",
-                title: "Event 3",
-                start: new Date(curr.getFullYear(), curr.getMonth(), curr.getDate() + 2, 15, 0, 0, 0),
-                end: new Date(curr.getFullYear(), curr.getMonth(), curr.getDate() + 2, 16, 0, 0, 0),
-                group: ["5"],
-                color: "green",
-            },
-        ];
+export async function fetchEvents() {
+    const colors = ["red", "purple", "yellow", "orange", "green", "blue"];
+    const days: number[] = [];
+
+    for (let index = dayjs().startOf("week").date(); index <= dayjs().endOf("week").date(); index++) {
+        days.push(index);
+    }
+
+    return await new Promise((res, _rej) => {
+        const result = Array.from({ length: Math.max(2, /* Math.floor(Math.random() * 5) */ 5) }, (_, index) => {
+            const id = index + 1;
+            const day = dayjs().set("date", days[Math.floor(Math.random() * 10) % 7]);
+            const start = day.set("hour", index).set("minute", 0).set("second", 0);
+            const end = day
+                .set("hour", index + 1)
+                .set("minute", 0)
+                .set("second", 0);
+
+            return {
+                id: id.toString(),
+                title: `Event ${id}`,
+                start,
+                end,
+                group: [day.day().toString()],
+                color: colors[Math.floor(Math.random() * 10) % 7],
+            };
+        });
 
         const tId = setTimeout(() => {
             res(result);
-
             clearTimeout(tId);
         }, 1000);
     });
